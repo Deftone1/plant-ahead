69a69db21592a9da9fb457d871666d903787d8f7
Paul Murray <paulmurray85@gmail.com>
2020-11-19 15:11:10 -0800

    fixed error with div

--- client/src/components/SavedPlants.js
+++ client/src/components/SavedPlants.js
@@ -1,66 +1,63 @@
 import React from "react";
 
-import Dropdown from 'react-bootstrap/Dropdown'
-
+import Dropdown from "react-bootstrap/Dropdown";
 
 function SavedPlants(props) {
-  const { plants, gardens, addplanttogarden} = props;
-  const gardenslist = gardens
-    .map((garden)=> 
-    <Dropdown.Item href="#/action-1"data-garden={garden.id}>{garden.name}</Dropdown.Item>
-    )
+  const { plants, gardens, addplanttogarden } = props;
+  const gardenslist = gardens.map((garden) => (
+    <Dropdown.Item href="#/action-1" data-garden={garden.id}>
+      {garden.name}
+    </Dropdown.Item>
+  ));
 
-  const plantList = plants
-    .map((plant) => (
-
-      <div >
-
-        <p className="card-text garden-text">{plant.name} <br></br></p>
-        <div className="d-flex justify-content-center">
-          <Dropdown>
-            <Dropdown.Toggle className="btn btn-primary btn-sm" id={plant._id}>
-              Add to Project
-
+  const plantList = plants.map((plant) => (
+    <div>
+      <p className="card-text garden-text">
+        {plant.name} <br></br>
+      </p>
+      <div className="d-flex justify-content-center">
+        <Dropdown>
+          <Dropdown.Toggle className="btn btn-primary btn-sm" id={plant._id}>
+            Add to Project
           </Dropdown.Toggle>
-            <Dropdown.Menu>
-             {gardens.map((garden)=> 
-          <Dropdown.Item href="#/action-1" onClick={() => addplanttogarden(garden._id,plant._id)}>{garden.name}</Dropdown.Item>
-            )}
-            </Dropdown.Menu>
-          </Dropdown>
+          <Dropdown.Menu>
+            {gardens.map((garden) => (
+              <Dropdown.Item
+                href="#/action-1"
+                onClick={() => addplanttogarden(garden._id, plant._id)}
+              >
+                {garden.name}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
 
-
-          <a href="#" className="btn btn-outline-danger btn-sm" id={plant._id} onClick={() => props.removeplant(plant._id)}>Remove Plant</a>
-
-        </div>
-
-
-
+        <a
+          href="#"
+          className="btn btn-outline-danger btn-sm"
+          id={plant._id}
+          onClick={() => props.removeplant(plant._id)}
+        >
+          Remove Plant
+        </a>
       </div>
-
-
-    ))
-
+    </div>
+  ));
 
   console.log(plants);
 
   return (
     <div className="col-sm-6">
+      <div className="card garden-card">
+        <div className="card-body shadow garden-body">
+          <h5 className="card-title garden-title">SAVED PLANTS</h5>
+          {plantList}
 
-    <div className="card garden-card">
-      <div className="card-body shadow garden-body">
-        <h5 className="card-title garden-title">SAVED PLANTS</h5>
-        {plantList}
-        
-      <button className="btn btn-primary">Clear Saved Plants</button>
-
+          <button className="btn btn-primary">Clear Saved Plants</button>
+        </div>
       </div>
     </div>
-
-
-  )
-};
-
-
+  );
+}
 
 export default SavedPlants;
