4893e3b06946d37c09df9540e94ae4cf9755c08a
ver Pasamic <vpasamic22@gmail.com>
2020-11-19 11:52:04 -0800

    remove button working

--- client/src/components/SavedPlants.js
+++ client/src/components/SavedPlants.js
@@ -5,7 +5,7 @@ function SavedPlants(props) {
   const {plants}=props;
   const plantList = plants
     .map((plant)=> (
-      <p className="card-text">{plant.name} <br></br><a href="#" className="btn btn-outline-success btn-sm" id={plant._id}>Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm"id={plant._id}>Remove Plant</a></p>
+      <p className="card-text">{plant.name} <br></br><a href="#" className="btn btn-outline-success btn-sm" id={plant._id}>Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm"id={plant._id} onClick={()=>props.removeplant(plant._id)}>Remove Plant</a></p>
     ))
 
 
--- client/src/util/API.js
+++ client/src/util/API.js
@@ -5,12 +5,14 @@ const getProtectedExample = () => axios.get("/api/protected");
 
 const savePlant =(plantData) => axios.post("/api/plants/", plantData)
 
-const getPlantbyid = (id) => axios.get("api/plants/user/"+id)
+const removeplantbyid = (id) => axios.delete("/api/plants/" +id)
+
+const getPlantbyuserid = (id) => axios.get("api/plants/user/"+id)
 // example api request for public data
 const getPublicExample = () => axios.get("/api/unprotected");
 
 const trefle = (plant) => axios.get("/api/protected/trefle/" +plant)
 
-const API = { getProtectedExample, getPublicExample, trefle, savePlant, getPlantbyid };
+const API = { getProtectedExample, getPublicExample, trefle, savePlant, getPlantbyuserid, removeplantbyid };
 
 export default API;
