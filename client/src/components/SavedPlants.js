fb489a255fbc780890b66cb0c4403864a3d5b175
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-11-19 14:33:36 -0800

    added dropdown menue

--- client/src/components/SavedPlants.js
+++ client/src/components/SavedPlants.js
@@ -1,26 +1,54 @@
 import React from "react";
 
+import Dropdown from 'react-bootstrap/Dropdown'
+
 
 function SavedPlants(props) {
-  const {plants}=props;
+  const { plants } = props;
   const plantList = plants
-    .map((plant)=> (
-      <p className="card-text garden-text">{plant.name} <br></br><a href="#" className="btn btn-outline-success btn-sm" id={plant._id}>Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm"id={plant._id} onClick={()=>props.removeplant(plant._id)}>Remove Plant</a></p>
+    .map((plant) => (
+
+      <div >
+
+        <p className="card-text garden-text">{plant.name} <br></br></p>
+        <div className="d-flex justify-content-center">
+          <Dropdown>
+            <Dropdown.Toggle className="btn btn-primary btn-sm" id={plant._id}>
+              Add to Project
+
+          </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Garden1</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Garden2</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Garden3</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+
+          <a href="#" className="btn btn-outline-danger btn-sm" id={plant._id} onClick={() => props.removeplant(plant._id)}>Remove Plant</a>
+
+        </div>
+
+
+
+      </div>
+
+
     ))
 
 
-
+  console.log(plants);
 
   return (
     <div className="col-sm-6">
-    <div className="card garden-card">
-      <div className="card-body shadow garden-body">
-        <h5 className="card-title garden-title">SAVED PLANTS</h5>
-        {plantList}
-        <a href="#" className="btn btn-primary">Clear Saved Plants</a>
+      <div className="card garden-card">
+        <div className="card-body shadow garden-body">
+          <h5 className="card-title garden-title">SAVED PLANTS</h5>
+          {plantList}
+          <a href="#" className="btn btn-primary">Clear Saved Plants</a>
+        </div>
       </div>
     </div>
-  </div>
 
 
   )
