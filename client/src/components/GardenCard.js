fe469a41d87f43360823a652c363edd76181a973
Paul Murray <paulmurray85@gmail.com>
2020-11-19 10:37:50 -0800

    fixed css font errors on separate pages

--- client/src/components/GardenCard.js
+++ client/src/components/GardenCard.js
@@ -4,13 +4,13 @@ function GardenCard(props) {
 
     return (
         <div className="col-sm-6">
-    <div className="card">
-      <div className="card-body shadow">
-        <h5 className="card-title">MY PROJECTS</h5>
-        <p className="card-text">Garden #1</p>
-        <p className="card-text">Garden #2</p>
-        <p className="card-text">Garden #3</p>
-        <p className="card-text">Garden #4</p>
+    <div className="card garden-card">
+      <div className="card-body shadow garden-body">
+        <h5 className="card-title garden-title">MY PROJECTS</h5>
+        <p className="card-text garden-text">Garden #1</p>
+        <p className="card-text garden-text">Garden #2</p>
+        <p className="card-text garden-text">Garden #3</p>
+        <p className="card-text garden-text">Garden #4</p>
         <a href="#" className="btn btn-primary">Clear Projects</a>
       </div>
     </div>
--- client/src/components/SavedPlants.js
+++ client/src/components/SavedPlants.js
@@ -4,13 +4,13 @@ function SavedPlants(props) {
 
   return (
     <div className="col-sm-6">
-    <div className="card">
-      <div className="card-body shadow">
-        <h5 className="card-title">SAVED PLANTS</h5>
-        <p className="card-text">Cactus <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
-        <p className="card-text">Rose Bush <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
-        <p className="card-text">Rubber Plant <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
-        <p className="card-text">Venus Fly Trap <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
+    <div className="card garden-card">
+      <div className="card-body shadow garden-body">
+        <h5 className="card-title garden-title">SAVED PLANTS</h5>
+        <p className="card-text garden-text">Cactus <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
+        <p className="card-text garden-text">Rose Bush <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
+        <p className="card-text garden-text">Rubber Plant <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
+        <p className="card-text garden-text">Venus Fly Trap <br></br><a href="#" className="btn btn-outline-success btn-sm">Add to Project</a><a href="#" className="btn btn-outline-danger btn-sm">Remove Plant</a></p>
         <a href="#" className="btn btn-primary">Clear Saved Plants</a>
       </div>
     </div>
