f807449d51a257d76241df4e1ec97a0871198258
Paul Murray <paulmurray85@gmail.com>
2020-11-20 13:21:14 -0800

    cleaned up code deleted unused components

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer(props) {
 
     return (
 
-        <footer className="bg-dark fixed-bottom col-lg-12 text-center">
-            <h1>Footer Goes here</h1>
+        <footer className="fixed-bottom col-lg-12 text-center">
+            <p>Copyright MoonGate. 2020</p>
 
         </footer>
     )
