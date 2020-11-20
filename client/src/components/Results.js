ae9751e8a4737da9ecca78e7b4751acac660bc6c
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-11-20 13:23:58 -0800

    add Toast

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../styles/Results.css";
+import { useEffect, useState } from "react";
 
 // changed this to a function not a variable
 function Results(props) {
@@ -7,6 +8,7 @@ function Results(props) {
     //  deconstructing props here and defining trees
     const { trees } = props;
     
+
 
     const plantList = trees
         .map((tree) => (
@@ -16,14 +18,14 @@ function Results(props) {
                     <img src={tree.image_url} className="card-img-top mx-auto border rounded img-thumbnail img-fluid" style={{ width: "250px", maxHeight: "200px", objectFit: "cover" }} alt={tree.name} />
                     <div className="card-body">
                         <div className="row">
-                        <div className="col">
-                            <h5 className="card-title">{tree.name}</h5>
-                            <p className="card-text">{tree.family}</p>
-                            <p className="card-text">{tree.genus}</p>
-                        </div>
-                        <div className="col">
-                          <button className="addPlanToList btn btn-primary" style={{fontSize:"15px"}} id={tree.id} onClick={(event) => props.handleSavedButton(event)}>Add to List</button>
-                        </div>
+                            <div className="col">
+                                <h5 className="card-title">{tree.name}</h5>
+                                <p className="card-text">{tree.family}</p>
+                                <p className="card-text">{tree.genus}</p>
+                            </div>
+                            <div className="col">
+                                <button className="addPlanToList btn btn-primary" style={{ fontSize: "15px" }} id={tree.id} onClick={(event) => props.handleSavedButton(event)}>Add to List</button>
+                            </div>
 
                         </div>
 
@@ -32,7 +34,7 @@ function Results(props) {
             </div>
 
         ));
-        
+
     return (
         <div className="col-md-11 mx-auto border shadow results-box" >
             <div className="row">
@@ -45,7 +47,7 @@ function Results(props) {
                     {plantList}
 
                 </div>
-
+               
             </div>
         </div>
 
