4e93703d242fdaa8eda18bfe6cce2fa2f5eb5e21
Visinigiri Aditya <61955750+adityakumar48@users.noreply.github.com>
2020-11-21 18:52:05 +0530

    Update SearchForm.js

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,52 +1,81 @@
 import React from "react";
 
 function SearchForm(props) {
-
   return (
-
-    <section className="row mainSection d-flex  justify-content-center align-items-center">
-      <div className="col-md-4 col-xs-2 border pr-5 pl-5 pt-2 shadow rounded"  >
-        <h4 className="text-center">Search For Your Plant Below:<br />
-              </h4><br />
-        <form >
+    <section className="row mainSection d-flex mt-2 mb-2  justify-content-center align-items-center">
+      <div className="col-md-4 col-xs-2 border pr-5 pl-5 pt-2 shadow rounded">
+        <h4 className="text-center">
+          Search For Your Plant Below:
+          <br />
+        </h4>
+        <br />
+        <form>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="commonName" value="option1" />
-            <label className="form-check-label" htmlFor="inlineCheckbox1">Common-Name</label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="commonName"
+              value="option1"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox1">
+              Common-Name
+            </label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="edible" value="option2" />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">Edible</label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="edible"
+              value="option2"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">
+              Edible
+            </label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="height" value="option2" />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">Height</label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="height"
+              value="option2"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">
+              Height
+            </label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="species" value="option2" />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">Species</label>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="species"
+              value="option2"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">
+              Species
+            </label>
           </div>
-          <br></br><br></br>
+          <br></br>
+          <br></br>
           <div className="form-group d-flex">
-            <input type="text"
+            <input
+              type="text"
               className="form-control"
               placeholder="Search for a plant"
               value={props.searchPlant}
               onChange={props.handleInputChange}
             />
-            <button type="submit"
+            <button
+              type="submit"
               className="btn1 btn-primary ml-4"
               onClick={props.handleFormSubmit}
-            >Submit</button>
+            >
+              Submit
+            </button>
           </div>
-
         </form>
-
-
-
       </div>
     </section>
-    )
-};
+  );
+}
 
-
-export default SearchForm
+export default SearchForm;
