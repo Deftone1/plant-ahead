0462ffb24a1f9eaa2be70a4371a21769f6576db5
Paul Murray <paulmurray85@gmail.com>
2020-11-23 14:21:30 -0800

    added image placeholder for empty search box

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,69 +1,78 @@
-import React from "react"
-import "../styles/Results.css"
+import React from "react";
+import "../styles/Results.css";
 import Plant from "../images/Plant.png";
+import PlantGrow from "../images/plantgrow.png";
 
 // changed this to a function not a variable
 function Results(props) {
+  //  deconstructing props here and defining trees
+  const { trees } = props;
 
-    //  deconstructing props here and defining trees
-    const { trees } = props;
+  const plantList = trees.map((tree) => (
+    <div className="col mb-4 results" key={tree.key}>
+      <div className="card">
+        {/* If there is no image use place holder */}
 
+        {!tree.image_url && (
+          <img
+            src={Plant}
+            className="card-img-top mx-auto border rounded img-thumbnail img-fluid mt-2"
+            style={{ width: "200px", maxHeight: "200px", objectFit: "contain" }}
+            alt={tree.name}
+          />
+        )}
 
+        {tree.image_url && (
+          <img
+            src={tree.image_url}
+            className="card-img-top mx-auto border rounded img-thumbnail img-fluid mt-2"
+            style={{ width: "250px", maxHeight: "200px", objectFit: "cover" }}
+            alt={tree.name}
+          />
+        )}
 
-    const plantList = trees
-        .map((tree) => (
-
-            <div className="col mb-4 results" key={tree.key} >
-                <div className="card" >
-                    {/* If there is no image use place holder */}
-
-                    {!tree.image_url && (<img src={Plant} className="card-img-top mx-auto border rounded img-thumbnail img-fluid mt-2" style={{ width: "200px", maxHeight: "200px", objectFit: "contain" }} alt={tree.name} />
-                    
-                    )
-                    }
-
-                    {tree.image_url && (<img src={tree.image_url} className="card-img-top mx-auto border rounded img-thumbnail img-fluid mt-2" style={{ width: "250px", maxHeight: "200px", objectFit: "cover" }} alt={tree.name} />
-                    )
-                    }
-
-                    <div className="card-body mx-auto">
-                        <div className="row">
-                            <div className="col">
-                                <h5 className="card-title">{tree.name}</h5>
-                                <p className="card-text">{tree.family}</p>
-                                <p className="card-text">{tree.genus}</p>
-                            </div>
-                            <div className="col">
-                                <button className="addPlanToList btn-primary add-to-list" style={{ fontSize: "15px" }} id={tree.id} onClick={(event) => props.handleSavedButton(event)}>Add to List</button>
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
+        <div className="card-body mx-auto">
+          <div className="row">
+            <div className="col">
+              <h5 className="card-title">{tree.name}</h5>
+              <p className="card-text">{tree.family}</p>
+              <p className="card-text">{tree.genus}</p>
             </div>
-
-        ));
-
-    return (
-        <div className="col-md-11 mx-auto border shadow results-box search"  >
-            <div className="row">
-                <div className="col">
-                    <h3 className="searchTitle search-results">Search Results:</h3>
-                </div>
-
-                {/* return card collection here */}
-                <div className="row row-cols-1 row-cols-md-3 searchResultImages pl-3" >
-                    {plantList}
-
-                </div>
-
+            <div className="col">
+              <button
+                className="addPlanToList btn-primary add-to-list"
+                style={{ fontSize: "15px" }}
+                id={tree.id}
+                onClick={(event) => props.handleSavedButton(event)}
+              >
+                Add to List
+              </button>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return trees.length === 0 ? (
+    <div className="col-md-11 mx-auto border shadow results-box">
+      <div className="row">
+        <img src={PlantGrow} alt="main screen growing plant" className="plantgrow"/>
+      </div>
+    </div>
+  ) : (
+    <div className="col-md-11 mx-auto border shadow results-box search">
+      <div className="row">
+        <div className="col">
+          <h3 className="searchTitle search-results">Search Results:</h3>
         </div>
 
-    )
-
-};
-export default Results
-
-
+        {/* return card collection here */}
+        <div className="row row-cols-1 row-cols-md-3 searchResultImages pl-3">
+          {plantList}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Results;
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,52 +1,80 @@
 import React from "react";
 
 function SearchForm(props) {
-
   return (
-
     <section className="row mainSection d-flex  justify-content-center align-items-center">
-      <div className="col-md-4 col-xs-2 border pr-5 pl-5 pt-2 shadow rounded search"  >
-        <h4 className="text-center">Search For Your Plant Below:<br />
-              </h4><br />
-        <form >
-          <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="commonName" value="option1" />
-            <label className="form-check-label" htmlFor="inlineCheckbox1">Common-Name</label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="edible" value="option2" />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">Edible</label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="height" value="option2" />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">Height</label>
-          </div>
-          <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="species" value="option2" />
-            <label className="form-check-label" htmlFor="inlineCheckbox2">Species</label>
-          </div>
-          <br></br><br></br>
+      <div className="col-md-4 col-xs-2 border pr-5 pl-5 pt-2 shadow rounded search">
+        <h4 className="text-center">
+          Search For Your Plant Below:
+          <br />
+        </h4>
+        <br />
+        <form>
+          
           <div className="form-group d-flex">
-            <input type="text"
+            <input
+              type="text"
               className="form-control"
               placeholder="Search for a plant"
               value={props.searchPlant}
               onChange={props.handleInputChange}
             />
-            <button type="submit"
+            <button
+              type="submit"
               className="btn1 btn-primary ml-4"
               onClick={props.handleFormSubmit}
-            >Submit</button>
+            >
+              Submit
+            </button>
           </div>
-
+          <div className="form-check form-check-inline pb-3">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="commonName"
+              value="option1"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox1">
+              Common-Name
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="edible"
+              value="option2"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">
+              Edible
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="height"
+              value="option2"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">
+              Height
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="species"
+              value="option2"
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox2">
+              Species
+            </label>
+          </div>
         </form>
-
-
-
       </div>
     </section>
-    )
-};
+  );
+}
 
-
-export default SearchForm
+export default SearchForm;
